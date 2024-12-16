d66a0ffa0b099a820e26888ddffb9596af985ee4
blackmaul17 <hikmalmaulana779988@gmail.com>
2024-12-16 19:13:34 +0700

    first

---
+++ components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+function HelloWorld() {
+  return (
+    <>
+        <h1>Hello, World!</h1>
+        <p>Selamat Belajar</p>    
+        <button>Login</button>
+    </>
+  )
+}
+
+export default HelloWorld
